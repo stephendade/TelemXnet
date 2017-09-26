be677db85b00b460989ef043c8f48b78685a3ab4
Stephen Dade <stephen_dade@hotmail.com>
2017-09-26 20:21:12 +1000

    Using localhost for clients

--- gcsClient.sh
+++ gcsClient.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 TelemXnet-Client.py --netid=fhr74kdh37snfy3idnche72ksndge82k --remote=192.168.0.101:16250 --localport=14650 --mode=gcs
+python3 TelemXnet-Client.py --netid=fhr74kdh37snfy3idnche72ksndge82k --remote=127.0.0.1:16250 --localport=14650 --mode=gcs
